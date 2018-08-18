e9eb4a4410b473152905d625084057549b0da477
Mamo <m.abeli@t4project.com>
2018-08-18 14:35:19 +0200

    MapVC UI - Conform to MKMapViewDelegate

--- Pixel city/Controllers/MapVC.swift
+++ Pixel city/Controllers/MapVC.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            mapView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func centerBtnPressed(_ sender: Any) {
+        
     }
+    
+}
 
-
+// Extension to handle Map View delegate
+extension MapVC: MKMapViewDelegate {
+    
 }
 
